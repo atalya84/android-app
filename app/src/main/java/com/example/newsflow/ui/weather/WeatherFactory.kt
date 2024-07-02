c99bb144bd1026d379eb016b8d02ed53b0f43ae9
atalya <email@org.com>
2024-07-02 04:12:48 +0300

    fix weather - ask for permissions

--- app/src/main/java/com/example/newsflow/ui/weather/WeatherFactory.kt
+++ app/src/main/java/com/example/newsflow/ui/weather/WeatherFactory.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class WeatherFactory(private val context: Context) : ViewModelProvider.Factory {
+class WeatherFactory() : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return WeatherViewModel(context) as T
+            return WeatherViewModel() as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
--- app/src/main/java/com/example/newsflow/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/newsflow/ui/weather/WeatherViewModel.kt
@@ -5,64 +5,57 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.LiveData
 import retrofit2.Response
 import android.Manifest
+import android.app.Activity
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.LocationServices
 import java.io.IOException
-import java.util.Locale
 import retrofit2.Callback
 import retrofit2.Call
 import com.example.newsflow.models.WeatherResponse
 import com.example.newsflow.networking.ApiConfig
-import com.google.android.gms.location.FusedLocationProviderClient
 
-class WeatherViewModel(private val context: Context) : ViewModel() {
+class WeatherViewModel() : ViewModel() {
 
     private val _weatherData = MutableLiveData<WeatherResponse>()
     val weatherData: LiveData<WeatherResponse> get() = _weatherData
     var errorMessage: String = ""
         private set
 
-    private val fusedLocationClient: FusedLocationProviderClient by lazy {
-        LocationServices.getFusedLocationProviderClient(context)
-    }
+    fun getCurrentLocationWeather(context: Context, activity: Activity) {
+        Log.d("getCurrentWeather", "got in")
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ) {
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
-    fun getCurrentLocationWeather() {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            onError("Location permission not granted")
-            return
-        }
-
-        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            location?.let {
-                val geocoder = Geocoder(context, Locale.getDefault())
-                try {
-                    val addresses: List<Address> = geocoder.getFromLocation(location.latitude, location.longitude, 1) ?: emptyList()
-                    if (addresses.isNotEmpty()) {
-                        val cityName: String = addresses[0].locality
-                        getWeatherData(cityName)
-                    } else {
-                        onError("Unable to get city name")
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    try {
+                        val lat = it.latitude
+                        val lon = it.longitude
+                        val client = ApiConfig.getApiService().getWeatherByCoordinates(latitude = lat, longitude = lon)
+                        handleApiResponse(client)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        onError("Geocoder Service Error")
                     }
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    onError("Geocoder Service Error")
-                }
-            } ?: onError("Location Error")
-        }.addOnFailureListener { e ->
-            e.printStackTrace()
-            onError("Failed to get Location")
+                } ?: onError("Location Error")
+            }.addOnFailureListener { e ->
+                e.printStackTrace()
+                onError("Failed to get Location")
+            }
         }
     }
 
     fun getWeatherData(city: String) {
-
         val client = ApiConfig.getApiService().getCurrentWeather(city = city)
+        handleApiResponse(client)
+    }
 
-        // Send API request using Retrofit
+    private fun handleApiResponse( client: Call<WeatherResponse>) {
         client.enqueue(object : Callback<WeatherResponse> {
 
             override fun onResponse(
